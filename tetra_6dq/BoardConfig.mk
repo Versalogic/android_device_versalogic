3bd67ccaf8c69a35a574ca0e116d15ba0b5d988c
Alan Cave <acave@versalogic.net>
2018-09-25 15:23:56 -0700

    Android 8 device layer

--- tetra_6dq/BoardConfig.mk
+++ tetra_6dq/BoardConfig.mk
@@ -9,48 +9,40 @@ include device/fsl/imx6/BoardConfigCommon.mk
 BUILD_TARGET_FS ?= ext4
 include device/fsl/imx6/imx6_target_fs.mk
 
-ifeq ($(BUILD_TARGET_DEVICE),sd)
-ADDITIONAL_BUILD_PROPERTIES += \
-                        ro.internel.storage_size=/sys/block/mmcblk1/size \
-                        ro.boot.storage_type=sd \
-                        ro.frp.pst=/dev/block/mmcblk0p12
 ifneq ($(BUILD_TARGET_FS),f2fs)
-TARGET_RECOVERY_FSTAB = device/versalogic/tetra_6dq/fstab_sd.freescale
 # build for ext4
+ifeq ($(PRODUCT_IMX_CAR),true)
+TARGET_RECOVERY_FSTAB = device/versalogic/tetra_6dq/fstab.freescale.car
 PRODUCT_COPY_FILES +=	\
-	device/versalogic/tetra_6dq/fstab_sd.freescale:root/fstab.freescale
+	device/versalogic/tetra_6dq/fstab.freescale.car:root/fstab.freescale
 else
-TARGET_RECOVERY_FSTAB = device/versalogic/tetra_6dq/fstab_sd-f2fs.freescale
-# build for f2fs
-PRODUCT_COPY_FILES +=	\
-	device/versalogic/tetra_6dq/fstab_sd-f2fs.freescale:root/fstab.freescale
-endif # BUILD_TARGET_FS
-else
-ADDITIONAL_BUILD_PROPERTIES += \
-                        ro.internel.storage_size=/sys/block/mmcblk3/size \
-                        ro.boot.storage_type=emmc \
-                        ro.frp.pst=/dev/block/mmcblk3p12
-ifneq ($(BUILD_TARGET_FS),f2fs)
 TARGET_RECOVERY_FSTAB = device/versalogic/tetra_6dq/fstab.freescale
-# build for ext4
 PRODUCT_COPY_FILES +=	\
 	device/versalogic/tetra_6dq/fstab.freescale:root/fstab.freescale
+endif # PRODUCT_IMX_CAR
 else
 TARGET_RECOVERY_FSTAB = device/versalogic/tetra_6dq/fstab-f2fs.freescale
 # build for f2fs
 PRODUCT_COPY_FILES +=	\
 	device/versalogic/tetra_6dq/fstab-f2fs.freescale:root/fstab.freescale
 endif # BUILD_TARGET_FS
-endif # BUILD_TARGET_DEVICE
 
+# Vendor Interface Manifest
+ifeq ($(PRODUCT_IMX_CAR),true)
+PRODUCT_COPY_FILES += \
+    device/versalogic/tetra_6dq/manifest_car.xml:vendor/manifest.xml
+else
+PRODUCT_COPY_FILES += \
+    device/versalogic/tetra_6dq/manifest.xml:vendor/manifest.xml
+endif
 
 TARGET_BOOTLOADER_BOARD_NAME := TETRA
 PRODUCT_MODEL := TETRA-MX6DQ
 
 TARGET_BOOTLOADER_POSTFIX := imx
+TARGET_DTB_POSTFIX := -dtb
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/fsl/imx6
-
 # UNITE is a virtual device.
 BOARD_WLAN_DEVICE            := UNITE
 WPA_SUPPLICANT_VERSION       := VER_0_8_UNITE
@@ -61,28 +53,10 @@ BOARD_HOSTAPD_DRIVER         := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB_BCM               := lib_driver_cmd_bcmdhd
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB_BCM        := lib_driver_cmd_bcmdhd
 
-BOARD_SUPPORT_BCM_WIFI  := false
-#for intel vendor
-ifeq ($(BOARD_WLAN_VENDOR),INTEL)
-BOARD_HOSTAPD_PRIVATE_LIB                := private_lib_driver_cmd
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB         := private_lib_driver_cmd
-WPA_SUPPLICANT_VERSION                   := VER_0_8_X
-HOSTAPD_VERSION                          := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB         := private_lib_driver_cmd_intel
-WIFI_DRIVER_MODULE_PATH                  := "/system/lib/modules/iwlagn.ko"
-WIFI_DRIVER_MODULE_NAME                  := "iwlagn"
-WIFI_DRIVER_MODULE_PATH                  ?= auto
-endif
-
-WIFI_DRIVER_FW_PATH_STA        := "/system/etc/firmware/bcm/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_P2P        := "/system/etc/firmware/bcm/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP         := "/system/etc/firmware/bcm/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA        := "/vendor/firmware/bcm/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P        := "/vendor/firmware/bcm/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP         := "/vendor/firmware/bcm/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM      := "/sys/module/bcmdhd/parameters/firmware_path"
-
-BOARD_MODEM_VENDOR := 
-
-USE_ATHR_GPS_HARDWARE := false
-USE_QEMU_GPS_HARDWARE := false
 
 #for accelerator sensor, need to define sensor type here
 BOARD_HAS_SENSOR := true
@@ -93,7 +67,6 @@ TARGET_SELECT_KEY := 28
 
 # we don't support sparse image.
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
-DM_VERITY_RUNTIME_CONFIG := true
 # uncomment below lins if use NAND
 #TARGET_USERIMAGES_USE_UBIFS = true
 
@@ -133,13 +106,33 @@ IMX_CAMERA_HAL_V3 := true
 #define consumer IR HAL support
 IMX6_CONSUMER_IR_HAL := false
 
-TARGET_BOOTLOADER_CONFIG := imx6q:imx6qtetraandroid_config 
+TARGET_BOOTLOADER_CONFIG := imx6q:mx6qtetraandroid_config 
 TARGET_BOARD_DTS_CONFIG := imx6q:imx6q-tetra.dtb
 
 BOARD_SEPOLICY_DIRS := \
        device/fsl/imx6/sepolicy \
        device/versalogic/tetra_6dq/sepolicy
 
-BOARD_SECCOMP_POLICY += device/versalogic/tetra_6dq/seccomp
+ifeq ($(PRODUCT_IMX_CAR),true)
+BOARD_SEPOLICY_DIRS += \
+     packages/services/Car/car_product/sepolicy \
+     device/generic/car/common/sepolicy
+endif
+
+PRODUCT_COPY_FILES +=	\
+       device/versalogic/tetra_6dq/ueventd.freescale.rc:root/ueventd.freescale.rc
+
+# Support gpt
+BOARD_BPT_INPUT_FILES += device/fsl/common/partition/device-partitions-7GB.bpt
+ADDITION_BPT_PARTITION = partition-table-14GB:device/fsl/common/partition/device-partitions-14GB.bpt \
+                         partition-table-28GB:device/fsl/common/partition/device-partitions-28GB.bpt
+
+# Vendor seccomp policy files for media components:
+PRODUCT_COPY_FILES += \
+       device/versalogic/tetra_6dq/seccomp/mediacodec-seccomp.policy:vendor/etc/seccomp_policy/mediacodec.policy \
+       device/versalogic/tetra_6dq/seccomp/mediaextractor-seccomp.policy:vendor/etc/seccomp_policy/mediaextractor.policy
+
+PRODUCT_COPY_FILES += \
+       device/versalogic/tetra_6dq/app_whitelist.xml:system/etc/sysconfig/app_whitelist.xml
 
 TARGET_BOARD_KERNEL_HEADERS := device/fsl/common/kernel-headers
--- tetra_6dq/tetra_6dq.mk
+++ tetra_6dq/tetra_6dq.mk
@@ -1,6 +1,7 @@
 # This is a FSL Android Reference Design platform based on i.MX6Q ARD board
 # It will inherit from FSL core product which in turn inherit from Google generic
 
+-include device/fsl/common/imx_path/ImxPathConfig.mk
 $(call inherit-product, device/fsl/imx6/imx6.mk)
 $(call inherit-product-if-exists,vendor/google/products/gms.mk)
 
@@ -20,14 +21,16 @@ PRODUCT_MANUFACTURER := versalogic
 
 PRODUCT_COPY_FILES += \
 	device/versalogic/tetra_6dq/init.rc:root/init.freescale.rc \
-        device/versalogic/tetra_6dq/init.i.MX6Q.rc:root/init.freescale.i.MX6Q.rc \
-        device/versalogic/tetra_6dq/init.i.MX6DL.rc:root/init.freescale.i.MX6DL.rc \
-	device/versalogic/tetra_6dq/init.i.MX6QP.rc:root/init.freescale.i.MX6QP.rc \
+	device/versalogic/tetra_6dq/init.imx6q.rc:root/init.freescale.imx6q.rc \
+	device/versalogic/tetra_6dq/init.imx6dl.rc:root/init.freescale.imx6dl.rc \
+	device/versalogic/tetra_6dq/init.imx6qp.rc:root/init.freescale.imx6qp.rc \
 
+PRODUCT_COPY_FILES += device/versalogic/tetra_6dq/init.freescale.emmc.rc:root/init.freescale.emmc.rc
+PRODUCT_COPY_FILES += device/versalogic/tetra_6dq/init.freescale.sd.rc:root/init.freescale.sd.rc
 # Audio
 USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
-	device/versalogic/tetra_6dq/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+	device/versalogic/tetra_6dq/audio_effects.conf:system/etc/audio_effects.conf \
 	device/versalogic/tetra_6dq/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
@@ -36,17 +39,12 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
 
 PRODUCT_COPY_FILES +=	\
-	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
-	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
+        $(LINUX_FIRMWARE_IMX_PATH)/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin    \
+        $(LINUX_FIRMWARE_IMX_PATH)/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
 # setup dm-verity configs.
-ifneq ($(BUILD_TARGET_DEVICE),sd)
- PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk3p5
- $(call inherit-product, build/target/product/verity.mk)
-else 
- PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk2p5
+ PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/by-name/system
  $(call inherit-product, build/target/product/verity.mk)
 
-endif
 
 # GPU files
 
@@ -72,17 +70,70 @@ PRODUCT_COPY_FILES += \
 	device/versalogic/tetra_6dq/required_hardware.xml:system/etc/permissions/required_hardware.xml
 
 PRODUCT_COPY_FILES += \
-    device/fsl-proprietary/gpu-viv/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+    $(FSL_PROPRIETARY_PATH)/fsl-proprietary/gpu-viv/lib/egl/egl.cfg:system/lib/egl/egl.cfg
+
+# HWC2 HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1-impl
+
+# Gralloc HAL
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
+# imx6 sensor HAL libs.
+PRODUCT_PACKAGES += \
+       sensors.imx6
+
+# Usb HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
+
+# WiFi HAL
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    wifilogd \
+    wificond
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
 
 PRODUCT_PACKAGES += \
     libEGL_VIVANTE \
     libGLESv1_CM_VIVANTE \
     libGLESv2_VIVANTE \
-    gralloc_viv.imx6 \
-    hwcomposer_viv.imx6 \
-    hwcomposer_fsl.imx6 \
     libGAL \
     libGLSLC \
     libVSC \
     libg2d \
     libgpuhelper
+
+PRODUCT_PACKAGES += \
+    Launcher3
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.internel.storage_size=/sys/block/bootdev_size
+
+PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/by-name/presistdata
